7b7002fd3b932722f430e9ae965c5e9472be3e64
YUVAN GROVER <122732388+coderyuvan@users.noreply.github.com>
2024-07-06 14:34:53 +0000

    dataType Interview Ques

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,8 @@
+// primitive
+// 7 types :string,number,boolean,null,undefined,symbol(unique bnana h value ko ),BigInt
+//Reference/Non-primitve
+//Array,objects,functions
+// type of null:object
+// type of bigint:undefined
+// type of array ,object :function
+// type of function :object function
