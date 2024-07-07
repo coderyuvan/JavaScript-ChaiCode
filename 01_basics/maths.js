9c92f3b7b3ff86cff75fcbf6bb0195fc5e1b380e
YUVAN GROVER <122732388+coderyuvan@users.noreply.github.com>
2024-07-07 11:21:36 +0000

    Maths_Projects

---
+++ 01_basics/maths.js
@@ -0,0 +1,4 @@
+const min=10;
+const max=20;
+console.log(Math.floor(Math.random()*(max-min+1)+min))
+//  to get random values between 2 number for ecxample in a dice game from 1  to 6 use this formula
