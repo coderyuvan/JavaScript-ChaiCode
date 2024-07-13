2f5d37d4b3c7b2c0de8a1ccc8352d7a8c1e140f1
YUVAN GROVER <122732388+coderyuvan@users.noreply.github.com>
2024-07-13 18:28:36 +0000

    hi

---
+++ dom/learning.js
@@ -0,0 +1,2 @@
+// queryselectorall node list dega jispe u can apply for each value[0,1,] aise lena h
+// get element by class name agr html colection mile to phle usko araay m convert kro Array.from()
